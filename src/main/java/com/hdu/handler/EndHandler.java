138dffc699477134c71adca753acead7e02618db
dzl20221020 <2537253351@qq.com>
2025-11-03 16:34:20 +0800

    [Feat] 添加了应用状态根据实验状态的自动化处理流程
    Signed-off-by: dzl20221020 <2537253351@qq.com>

---
+++ src/main/java/com/hdu/handler/EndHandler.java
@@ -0,0 +1,35 @@
+package com.hdu.handler;
+
+import com.hdu.config.ExperimentProperties;
+import com.hdu.lister.DataLister;
+import com.hdu.service.IExperimentService;
+import com.hdu.utils.mqtt.MqttAcceptClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+@Slf4j
+public class EndHandler {
+    @Resource
+    private MqttAcceptClient mqttAcceptClient;
+
+    @Resource
+    DataLister dataLister;
+
+    @Resource
+    IExperimentService experimentService;
+
+    // 私有构造方法，防止外部实例化
+    private EndHandler(){}
+
+    public void handleEndState() {
+        log.info("实验状态处于结束，开始持久化数据");
+        mqttAcceptClient.unsubscribe("/test");
+        dataLister.disable();
+
+        // 执行数据持久化逻辑
+        experimentService.endExperiment(ExperimentProperties.experimentId);
+    }
+}
